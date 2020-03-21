6d33a96404680fe603b79f7b2303d175553002a3
Steven Leonardo <stevenleonardo@knights.ucf.edu>
2020-03-21 16:03:02 -0400

    updated comments

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 $(document).ready(function() {
-
+//event listener for the button
  $("#button").on("click", function(){
+   //stops page from refreshing
    event.preventDefault()
+   //takes the value from the input text
   var city = $("#City").val();
   console.log(city)
 
-
+//data base where information is stored
   var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=6b36709c3454a3dde3566cc35c961d4a"
   var queryURL2 = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&APPID=6b36709c3454a3dde3566cc35c961d4a"
-
+//places on value in the storage
   localStorage.setItem(city, city)
 
- 
+ //makes a call to the html
   $.ajax({
     url: queryURL,
     method: "GET"
@@ -19,25 +21,34 @@ $(document).ready(function() {
     var city =  response.name
     console.log(response)
     console.log(queryURL)
-    
+    //clears the div
       $("#main-city").empty()
+      //set the div to a local variable
     var mainCity = $("#main-city")
+    //creates h1 text that shows the current city and date
     var date = $("<h1>").text(city + "(" + moment().format('LL') + ")");
+    //places the information on the page
     mainCity.append(date);
-
+    //place the data from the object into a local variable
     var temperature = response.main.temp
+    //creates a p tage and places the info into the tag
     var temp = $("<p>").text("temperature: " + temperature);
+    //places the infomation onto the page
     mainCity.append(temp);
-
+    //places the data from the object into a local variable
     var humdity = response.main.humidity
+    //creates a p tage with the information obtained
     var hum = $("<p>").text("humidity: " + humdity + "%")
+    //places that information on the page
     mainCity.append(hum)
-
+    // obtian the wind speed from the object
     var wind = response.wind.speed
+    // creates a p tag and places the content into that tag
     var speed = $("<p>").text("wind speed: " + wind + "mph")
+    //places that content onto the page
     mainCity.append(speed)
   });
-
+  //making a second call to the html for the forecast
   $.ajax({
     url: queryURL2,
     method: "GET"
@@ -48,9 +59,11 @@ $(document).ready(function() {
 
   renderCities()
  })
-
+  //function for the local storage
   function renderCities(){
+    //empties out the div
     $("#cities").empty();
+    //loop that runs through the information in the local storage
     for (var i = 0; i < localStorage.length; i++) {
       var cities = localStorage.getItem(localStorage.key(i));
       var textCities = $("<p>").text(cities)
