4106ea7762e2b37a00411f56aba53f5e4186d515
Steven Leonardo <stevenleonardo@knights.ucf.edu>
2020-03-21 14:16:49 -0400

    working on js

--- app.js
+++ app.js
@@ -1,17 +1,30 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
-    var city = "orlando"
-    var apiKeys = "6b36709c3454a3dde3566cc35c961d4a"
-
-    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + apiKeys;
-    var queryURL2 = 
+  var city = $("#city").val();
 
 
+  var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=6b36709c3454a3dde3566cc35c961d4a"
+  var queryURL2 = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&APPID=6b36709c3454a3dde3566cc35c961d4a"
 
- $.ajax({
+
+
+ 
+  $.ajax({
     url: queryURL,
     method: "GET"
-  }).then(function(response) {
-      console.log(response)
-    }
+  }).then(function (response) {
+    console.log(response)
+    console.log(queryURL)
+  });
+
+  $.ajax({
+    url: queryURL2,
+    method: "GET"
+  }).then(function (response) {
+    console.log(response)
+    console.log(queryURL2)
+  });
+
+
+
 });
