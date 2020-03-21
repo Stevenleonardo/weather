5e50f8a95681a8888da55307b7e6e37ac656f6ef
Steven Leonardo <stevenleonardo@knights.ucf.edu>
2020-03-21 14:51:27 -0400

    still working on js

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 $(document).ready(function() {
 
+ $("#button").on("click", function(){
+   event.preventDefault()
   var city = $("#city").val();
+  console.log(city)
 
 
   var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=6b36709c3454a3dde3566cc35c961d4a"
   var queryURL2 = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&APPID=6b36709c3454a3dde3566cc35c961d4a"
 
-
+  localStorage.getItem(city, city)
 
  
   $.ajax({
@@ -15,6 +18,16 @@ $(document).ready(function() {
   }).then(function (response) {
     console.log(response)
     console.log(queryURL)
+    
+      $("#main-city").empty()
+    var mainCity = $("#main-city")
+    city = response.name;
+    var date = $("<h1>").text(city + "(" + moment().format('LL') + ")");
+    mainCity.append(date);
+
+    var temperature = response.main.temp
+    var temp = $("<p>").text("temperature: " + temperature);
+    mainCity.append(temp);
   });
 
   $.ajax({
@@ -25,6 +38,17 @@ $(document).ready(function() {
     console.log(queryURL2)
   });
 
+  renderCities()
+ })
 
-
+  function renderCities(){
+    $("#cities").empty();
+    for (var i = 0; i < localStorage.length; i++) {
+      var cities = localStorage.getItem(localStorage.key(i));
+      var textCities = $("<p>").text(cities)
+      $("#cities").prepend(textCities);
+      
+    }
+  }
+  
 });
